a56f4ab3b5da13058a932446bf10c542a95a6c6a
MartinColding <33082611+MartinColding@users.noreply.github.com>
2017-10-26 22:20:16 +0200

    Js
    test

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-(function(){
+(function( ){
     var app = angular.module('booking', []);
     
-    app.controller('BookController', function(){
+    app.controller( 'BookController', function(){
+        this.product = lokale;
        
         
     });
-});
+    var lokale = {
+        name:'Lokale'
+        number: A1 12,
+        description: '. . . '
+    }
+})();
 
