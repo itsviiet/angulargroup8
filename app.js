fb9bfd24e90a84ffa604d42cbc728812f439197c
pern8254 <33058628+pern8254@users.noreply.github.com>
2017-10-29 21:53:57 +0100

    nav

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     var app = angular.module('basketball', []);
 
     app.controller('BasketController', function(){
-        this.product = basket;
+        this.products = basket;
     });
 
     app.controller("ReviewController", function(){
