696b43d8202f5a0a7ae2c8f5454254d19221e072
Viet Nguyen <33082420+itsvitte@users.noreply.github.com>
2017-10-29 12:14:17 +0100

    Updates
    Hallooooooooo

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-var app = angular.module('booking', []);
+(function(){
+    var app = angular.module('basketball', []);
+
+    app.controller('BasketController', function(){
+        this.product = basket;
+    });
+
+    app.controller("ReviewController", function(){
+
+      this.review = {};
+
+      this.addReview = function(product){
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+      };
+    });
+
+    var basket = [{
+      place:'Havnen',
+      number: 12,
+      description: '. . . ',
+      reviews: [{
+        field: "Havnen",
+        stars: 5,
+        body: "Ok place",
+        author: "123@gmail.com"
+      }]
+    },
+    {
+      place: 'Random',
+      number: 08,
+      description: '...',
+      reviews: [{
+        field: "random",
+        stars: 4,
+        body: "Ok place",
+        author: "123@gmail.com"
+      }]
+    },
+    {
+      place: 'Forrest',
+      number: 08,
+      description: '...',
+      reviews: [{
+        field: "forrest",
+        stars: 3,
+        body: "Ok place",
+        author: "123@gmail.com"
+      }]
+    }];
+})();
