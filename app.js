152f9ca47853583c7f21867a52712fde8784ace2
Viet Nguyen <33082420+itsvitte@users.noreply.github.com>
2017-10-31 18:37:17 +0100

    ..

--- app.js
+++ app.js
@@ -6,13 +6,13 @@
     });
 
     app.controller("ReviewController", function(){
+
       this.review = {};
 
       this.addReview = function(product){
         this.review.createdOn = Date.now();
         product.reviews.push(this.review);
         this.review = {};
-          console.log("submit");
       };
 
     });
